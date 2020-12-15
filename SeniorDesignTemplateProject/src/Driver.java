4505a1654f3ba4bb96f3c4d43b90f356c4f6d074
Courtney <ccrawford21@jcu.edu>
2020-12-15 18:12:20 -0500

    2020-12-15T18:12:19.882

--- SeniorDesignTemplateProject/src/Driver.java
+++ SeniorDesignTemplateProject/src/Driver.java
@@ -2,6 +2,8 @@ public class Driver {
 	public static void main(String[] args) {
 		System.out.println("Testing date time for autocommit message!");
 	
+		
+		System.out.println("another change");
 		//some changes here wrgjwkrngaew
 	
 	}
