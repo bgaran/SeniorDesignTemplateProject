b4a51409bf06f1f5d9e0bd6352d5a3d8c381d9b3
Breanna <bgaran21@jcu.edu>
2020-12-17 22:31:17 -0500

    2020-12-17T22:31:17.612638

--- SeniorDesignTemplateProject/src/Driver.java
+++ SeniorDesignTemplateProject/src/Driver.java
@@ -2,5 +2,7 @@ public class Driver {
 	public static void main(String[] args) {
 		System.out.println("here is an example");
 		System.out.println("test outomatic push");
+		
+		//change to test push message
 	}
 }
